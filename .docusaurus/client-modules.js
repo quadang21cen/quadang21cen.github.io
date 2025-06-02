af8d7c44e77762b8d4dec347b251f6f0c7edb704
quadang21cen <quadang21cen@gmail.com>
2025-06-02 22:55:59 +0700

    [UPDATE] Blog

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/Users/quach/Desktop/Github/quadang21cen.github.io/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/Users/quach/Desktop/Github/quadang21cen.github.io/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/quach/Desktop/Github/quadang21cen.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/quach/Desktop/Github/quadang21cen.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/quach/Desktop/Github/quadang21cen.github.io/src/css/custom.css"),
+];
